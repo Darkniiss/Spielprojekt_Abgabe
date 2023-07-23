92e118a9c97a6ad0eb1cc525855847082c354c3e
Finn Leo <finn.elias.leo@gmail.com>
2023-07-23 15:08:59 +0200

    Added simple Attack method to Player & Enemy

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,29 +4,77 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float chaseSpeed;
+    [SerializeField] private float idleSpeed;
+    [SerializeField] public float healthPoints;
+    [SerializeField] private GameObject enemyWeapon;
 
+    private PlayerController player;
     private Rigidbody2D enemyRb;
+    private SpriteRenderer enemySprite;
+    private SpriteRenderer enemyWeaponSprite;
     private bool followsPlayer;
     private bool randomChosen;
+    private bool isFighting;
     private float timePassed;
     private float rndX;
     private float rndY;
 
+
     private void Awake()
     {
         enemyRb = GetComponent<Rigidbody2D>();
+        enemySprite = GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        enemyWeaponSprite = enemyWeapon.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         timePassed += Time.deltaTime;
 
+        if(healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!followsPlayer)
+        {
+        IdleMovement();
+        }
+        else if(isFighting)
+        {
+            Attack();
+        }
+
+        FlipSprite();
+
+    }
+
+    public void FlipSprite()
+    {
+        if (enemyRb.velocity.x > 0)
+        {
+            enemySprite.flipX = false;
+            enemyWeaponSprite.flipX = true;
+            enemyWeapon.transform.position = transform.position + new Vector3(0.5f, 0f, 0f);
+            enemyWeapon.transform.rotation = Quaternion.Euler(0f, 0f, -25f);
+        }
+        else if (enemyRb.velocity.x < 0)
+        {
+            enemySprite.flipX = true;
+            enemyWeaponSprite.flipX = false;
+            enemyWeapon.transform.position = transform.position + new Vector3(-0.5f, 0f, 0f);
+            enemyWeapon.transform.rotation = Quaternion.Euler(0f, 0f, 25f);
+        }
+    }
+
+    private void IdleMovement()
+    {
         if (!randomChosen)
         {
             rndX = Random.Range(-1, 2);
@@ -35,24 +83,33 @@ public class EnemyBehavior : MonoBehaviour
             randomChosen = true;
         }
 
-        if (!followsPlayer && timePassed > 2)
-        {
-            enemyRb.velocity = new Vector2(rndX, rndY) * moveSpeed * Time.deltaTime;
-        }
-
-        if (timePassed > 3 && !followsPlayer)
+        if (timePassed > 3)
         {
             enemyRb.velocity = Vector2.zero;
             timePassed = 0;
             randomChosen = false;
         }
+        else if (timePassed > 2)
+        {
+            enemyRb.velocity = new Vector2(rndX, rndY) * idleSpeed * Time.deltaTime;
+        }
+        
+    }
+
+    private void Attack()
+    {
+        if(timePassed > 2)
+        {
+            player.healthPoints--;
+            timePassed = 0;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !isFighting)
         {
-            enemyRb.velocity = (collision.transform.position - transform.position) * moveSpeed * Time.deltaTime;
+            enemyRb.velocity = (collision.transform.position - transform.position) * chaseSpeed * Time.deltaTime;
             followsPlayer = true;
         }
     }
@@ -62,6 +119,23 @@ public class EnemyBehavior : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             followsPlayer = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isFighting = true;
+            timePassed = 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isFighting = false;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    [SerializeField] private GameObject weapon;
-    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private GameObject playerWeapon;
+    
+    private Rigidbody2D playerRb;
+    private SpriteRenderer playerSprite;
 
-    private SpriteRenderer sprite;
+    private EnemyBehavior enemyFought;
 
     private Vector2 moveVec;
+    private float timePassed;
+    private bool isFighting;
+    public float healthPoints = 10f;
 
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     void Start()
@@ -26,24 +31,59 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        timePassed += Time.deltaTime;
+
         playerRb.velocity = (moveVec * moveSpeed * Time.deltaTime);
 
+        
+
+        FlipSprite();
+    }
+
+    public void FlipSprite()
+    {
         if (moveVec.x > 0)
         {
-            sprite.flipX = false;
-            weapon.transform.position = transform.position + new Vector3(0.5f, 0f, 0f);
-            weapon.transform.rotation = Quaternion.Euler(0f, 0f, -25f);
+            playerSprite.flipX = false;
+            playerWeapon.transform.position = transform.position + new Vector3(0.5f, 0f, 0f);
+            playerWeapon.transform.rotation = Quaternion.Euler(0f, 0f, -25f);
         }
-        else if (moveVec.x < 0) 
+        else if (moveVec.x < 0)
         {
-            sprite.flipX = true;
-            weapon.transform.position = transform.position + new Vector3(-0.5f, 0f, 0f);
-            weapon.transform.rotation = Quaternion.Euler(0f, 0f, 25f);
+            playerSprite.flipX = true;
+            playerWeapon.transform.position = transform.position + new Vector3(-0.5f, 0f, 0f);
+            playerWeapon.transform.rotation = Quaternion.Euler(0f, 0f, 25f);
         }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveVec = context.ReadValue<Vector2>();
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if(timePassed > 2 && isFighting)
+        {
+            enemyFought.healthPoints -= 2;
+            timePassed = 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemyFought = collision.gameObject.GetComponent<EnemyBehavior>();
+            isFighting = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            isFighting = false;
+        }
     }
 }
