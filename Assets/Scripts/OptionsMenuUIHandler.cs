e64d72ad9ce57836e3542158d2499ff3b956b961
Finn Leo <finn.elias.leo@gmail.com>
2023-07-27 17:01:26 +0200

    Added Resolution dropdown

--- Assets/Scripts/OptionsMenuUIHandler.cs
+++ Assets/Scripts/OptionsMenuUIHandler.cs
@@ -9,8 +9,34 @@ public class OptionsMenuUIHandler : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject optionsMenu;
-    
     [SerializeField] private AudioMixer mainMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        List<string> resOptions = new List<string>();
+
+        int curResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resOption = resolutions[i].width + "x" + resolutions[i].height;
+            resOptions.Add(resOption);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                curResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = curResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void BackToMenu()
     {
@@ -31,5 +57,10 @@ public class OptionsMenuUIHandler : MonoBehaviour
     public void SetFullscreen(bool _isFullscreen)
     {
         Screen.fullScreen = _isFullscreen;
+    }
+
+    public void SetResolution(int _resolution)
+    {
+        Screen.SetResolution(resolutions[_resolution].width, resolutions[_resolution].height, Screen.fullScreen);
     }
 }
